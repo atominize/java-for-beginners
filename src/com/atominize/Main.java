f68c6fc11b1e0c53bcbf1354a38760c5d534ed77
Robert Acquah <atominize@Roberts-MacBook.local>
2020-05-19 10:41:05 +0000

    scope of variables

--- src/com/atominize/Main.java
+++ src/com/atominize/Main.java
@@ -103,10 +103,24 @@ public class Main {
 //        }
 
         /* Example 15 */
-        double x, y, z;
-        x = 3;
-        y = 4;
-        z = Math.sqrt(x*x + y*y);
-        System.out.println("Hypotenuse is " + z);
+//        double x, y, z;
+//        x = 3;
+//        y = 4;
+//        z = Math.sqrt(x*x + y*y);
+//        System.out.println("Hypotenuse is " + z);
+
+        /* Example 16 */
+        int x; // known to all code within main
+        x = 10;
+
+        if (x == 10) { // start new scope
+            int y = 20; // known only to this block
+            // x and y both known here.
+            System.out.println("x and y: " + x + " " + y);
+            x = y * 2;
+        }
+        // y = 100; // Error! y not known here
+        // x is still known here.
+        System.out.println("x is " + x);
     }
 }
